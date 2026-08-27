5e9f4f32f4a1621aab95a7b39fa1c8826583ca8c
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-08-27 13:07:15 +0900

    Refresh PWA cache for fixed form layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v3'
+const CACHE = 'flow-shell-v4'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
