07208cd9bec8fd6ca2a1a25a07b8c62b485e9c9f
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-08-27 20:38:36 +0900

    Refresh PWA cache after bulk interaction fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v15'
+const CACHE = 'flow-shell-v16'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
