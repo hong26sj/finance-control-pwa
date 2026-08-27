8920c54fd02c8428656a83be878dd4cea52dafa0
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-08-27 18:49:30 +0900

    Refresh PWA cache after mobile fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v10'
+const CACHE = 'flow-shell-v11'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
