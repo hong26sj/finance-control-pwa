4bad5e70f652f04908e3334186798aec47c26302
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-08-29 15:27:06 +0900

    Refresh PWA cache for unified transaction editor

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v28'
+const CACHE = 'flow-shell-v29'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
