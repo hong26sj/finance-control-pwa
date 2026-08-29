1f79bb07bc135bc547596543f3048f469b86a60c
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-08-29 16:50:02 +0900

    Refresh PWA cache for calendar warning and editor sizing

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v29'
+const CACHE = 'flow-shell-v30'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
