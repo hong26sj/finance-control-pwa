69ff984a32dc477bf1dc9c1589a77290ec76908a
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-08-30 11:24:00 +0900

    Refresh PWA cache for faster Drive loading

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v33'
+const CACHE = 'flow-shell-v34'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
