2f83c68393e36696b851682d3cb0b89cbb512b32
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-09-01 20:36:48 +0900

    Refresh PWA cache for fixed cost grid alignment

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v40'
+const CACHE = 'flow-shell-v41'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
