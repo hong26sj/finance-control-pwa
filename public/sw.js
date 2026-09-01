7d2a7ecbeb82b0c6a540eb80c1061064d9c3b43a
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-09-01 10:14:03 +0900

    Refresh PWA cache for fixed cost formatting

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v37'
+const CACHE = 'flow-shell-v38'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
