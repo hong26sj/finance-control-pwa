c42a5046f07f53e46914d1a89039a9aced3fbc65
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-09-02 23:45:39 +0900

    Refresh PWA cache for transaction sync fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-shell-v44'
+const CACHE = 'flow-shell-v45'
 const ROOT = new URL('./', self.registration.scope).pathname
 const SHELL = [ROOT, `${ROOT}offline/`, `${ROOT}manifest.webmanifest`, `${ROOT}icon.svg`]
 
